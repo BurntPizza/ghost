546ae8730dddf654110ea53619b1017f50739080
Josh <xburntpizza@gmail.com>
2014-10-16 23:28:39 -0400

    More builtins and refactoring

--- Ghost/src/ghost/Ghost.java
+++ Ghost/src/ghost/Ghost.java
@@ -83,18 +83,18 @@ public class Ghost {
 		functions = new HashMap<>();
 		Map<String, Consumer<Stack>> map = functions;
 		
-		map.put("dup", s -> s.push(s.peek().copy()));
+		map.put("dup", s -> s.peek().copy().accept(s));
 		map.put("drop", s -> s.pop());
 		map.put("print", s -> System.out.println(s.peek()));
 		map.put("dump", s -> System.out.println(s));
 		map.put("exit", s -> System.exit(0));
 		map.put("clear", s -> s.clear());
 		map.put("apply", s -> s.popQuote().value().forEach(word -> word.accept(s)));
-		map.put("+", s -> s.push(new Int(s.popInt().value() + s.popInt().value())));
-		map.put("*", s -> s.push(new Int(s.popInt().value() * s.popInt().value())));
-		map.put("quote", s -> s.push(new Quote(Arrays.asList(s.pop()))));
+		map.put("+", s -> new Int(s.popInt().value() + s.popInt().value()).accept(s));
+		map.put("*", s -> new Int(s.popInt().value() * s.popInt().value()).accept(s));
+		map.put("quote", s -> new Quote(Arrays.asList(s.pop())).accept(s));
 		map.put("if", map.get("apply"));
-		map.put("typeof", s -> s.push(new Text(s.pop().getClass().getSimpleName())));
+		map.put("typeof", s -> new Text(s.pop().getClass().getSimpleName()).accept(s));
 		map.put("call", s -> {
 			String name = s.popText().value();
 			Consumer<Stack> c = functions.get(name);
@@ -105,7 +105,7 @@ public class Ghost {
 		map.put("reverse", s -> {
 			Quote q = s.popQuote();
 			Collections.reverse(q.value());
-			s.push(q);
+			q.accept(s);
 		});
 		map.put("zip", s -> {
 			Quote q2 = s.popQuote(), q1 = s.popQuote();
@@ -114,9 +114,9 @@ public class Ghost {
 			for (int i = 0; i < q1.value().size(); i++) {
 				q1.value().set(i, new Quote(Arrays.asList(q1.value().get(i), q2.value().get(i))));
 			}
-			s.push(q1);
+			q1.accept(s);
 		});
-		map.put("foreach", s -> {
+		map.put("map", s -> {
 			List<Word> func = s.popQuote().value();
 			Quote q = s.popQuote();
 			for (int i = 0; i < q.value().size(); i++) {
@@ -124,20 +124,20 @@ public class Ghost {
 				func.forEach(word -> word.accept(s));
 				q.value().set(i, s.pop());
 			}
-			s.push(q);
+			q.accept(s);
 		});
 		map.put("rot3", s -> {
 			Word w3 = s.pop(), w2 = s.pop(), w1 = s.pop();
-			s.push(w2);
-			s.push(w3);
-			s.push(w1);
+			w2.accept(s);
+			w3.accept(s);
+			w1.accept(s);
 		});
 		
 		Quote _true = new Quote("drop apply");
 		Quote _false = new Quote("swap drop apply");
 		
-		map.put("true", s -> s.push(_true));
-		map.put("false", s -> s.push(_false));
+		map.put("true", s -> _true.accept(s));
+		map.put("false", s -> _false.accept(s));
 		
 		map.put(">", s -> {
 			Integer i2 = s.popInt().value(), i1 = s.popInt().value();
@@ -152,24 +152,24 @@ public class Ghost {
 			List<Word> list = new ArrayList<>();
 			list.addAll(q1.value());
 			list.addAll(q2.value());
-			s.push(new Quote(list));
+			new Quote(list).accept(s);
 		});
 		map.put("join", s -> {
 			Word w2 = s.pop(), w1 = s.pop();
-			s.push(new Quote(Arrays.asList(w1, w2)));
+			new Quote(Arrays.asList(w1, w2)).accept(s);
 		});
 		map.put("-", s -> {
 			Integer i2 = s.popInt().value(), i1 = s.popInt().value();
-			s.push(new Int(i1 - i2));
+			new Int(i1 - i2).accept(s);
 		});
 		map.put("/", s -> {
 			Integer i2 = s.popInt().value(), i1 = s.popInt().value();
-			s.push(new Int(i1 / i2));
+			new Int(i1 / i2).accept(s);
 		});
 		map.put("swap", s -> {
 			Word w1 = s.pop(), w2 = s.pop();
-			s.push(w1);
-			s.push(w2);
+			w1.accept(s);
+			w2.accept(s);
 		});
 		Consumer<Stack> apply = map.get("apply");
 		map.put("filter", s -> {
@@ -179,15 +179,29 @@ public class Ghost {
 			int pos = 0;
 			for (int i = 0; i < list.size(); i++) {
 				Word w = list.get(i);
-				s.push(w);
-				s.push(pred);
+				w.accept(s);
+				pred.accept(s);
 				apply.accept(s);
 				if (s.pop() == _true)
 					list.set(pos++, w);
 			}
 			for (int i = list.size(); --i >= pos;)
 				list.remove(i);
-			s.push(data);
+			data.accept(s);
+		});
+		map.put("foreach", s -> {
+			List<Word> func = s.popQuote().value();
+			Quote q = s.popQuote();
+			for (int i = 0; i < q.value().size(); i++) {
+				q.value().get(i).accept(s);
+				func.forEach(word -> word.accept(s));
+			}
+		});
+		map.put("pop", s -> {
+			Quote q = s.popQuote();
+			List<Word> words = q.value();
+			q.accept(s);
+			new Quote(Arrays.asList(words.remove(words.size() - 1))).accept(s);
 		});
 		map.put("def", s -> {
 			String name = s.popText().value();
@@ -196,10 +210,12 @@ public class Ghost {
 		});
 		
 		Stack s = new Stack();
-		interpret("[[quote] foreach] split def", w -> w.accept(s));
-		interpret("[[] swap [compose 0] foreach drop] merge def", w -> w.accept(s));
-		//interpret("", w -> w.accept(s)); // add more defs
-		
+		interpret("[[quote] map] split def", w -> w.accept(s));
+		interpret("[[] swap [compose] foreach] merge def", w -> w.accept(s));
+		interpret("[zip merge] interleave def", w -> w.accept(s));
+		interpret("[swap [drop dup] map merge swap drop] fill def", w -> w.accept(s));
+		// eh why not
+		interpret("[dup [drop 1] [dup 2 - fib swap 1 - fib +] rot3 3 < if] fib def", w -> w.accept(s));
 		return map;
 	}
 	
